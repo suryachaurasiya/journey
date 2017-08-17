c0e6c282efd29d13931a1a1e844aed53e7626dad
vijay kumar <vijay.kumar@easypolicy.com>
2017-08-17 16:02:22 +0530

    fygh

--- Product-Journey/src/healthProduct/HealthProductJourneyWithExcel.java
+++ Product-Journey/src/healthProduct/HealthProductJourneyWithExcel.java
@@ -55,7 +55,7 @@ public class HealthProductJourneyWithExcel extends ExtentBaseClass {
 
 		try {
 			test = extent.createTest("Click on Health Link on top bar", "Health product page should be open with form");
-			WebElement health = driver.findElement(uimap.getLocator("health_product"));
+			WebElement health =driver.findElement(uimap.getLocator("health_product"));
 			health.click();
 			Thread.sleep(1000);
 			if (driver.getTitle().contains("Health Insurance")) {
